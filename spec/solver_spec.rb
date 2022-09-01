0d15f58a045a862d179f5f94a8114bb80f91d716
Fabien <fabienswift@gmail.com>
2022-09-01 16:52:38 -0400

    Add last test

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -8,4 +8,19 @@ describe Solver do
     it 'should return the reversed string' do
         expect (reverse('hello')).to eq('olleh')
     end
+
+    context 'Fizzbuzz test' do
+        it 'should return fizz if N is divisible by 3' do
+            expect (fizzbuzz(6)).to be('fizz')
+        end
+        it 'should return buzz if N is divisible by 5' do
+            expect (fizzbuzz(10)).to be('buzz')
+        end
+        it 'should return fizzbuzz if N is divisible by 3 and 5' do
+            expect (fizzbuzz(15)).to be('fizzbuzz')
+        end
+        it "should return 'N' if N is not divisible by 3 or 5" do
+            expect (fizzbuzz(8)).to be('8')
+        end
+    end
 end
