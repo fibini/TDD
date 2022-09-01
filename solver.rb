a938e59128c212addcbca37974d2cc95581d5365
Fabien <fabienswift@gmail.com>
2022-09-01 18:36:02 -0400

    Create Class for previous tests

--- solver.rb
+++ solver.rb
@@ -2,15 +2,29 @@
 
 # comment
 class Solver
+  # attr_reader :reverse, :fizzbuzz
   def factorial(num)
-    puts num
+    if num.zero?
+      1
+    elsif num.negative?
+      raise StandardError
+    end
+    (1..num).inject(:*) || 1
   end
 
   def reverse(string)
-    puts string
+    string.reverse
   end
 
   def fizzbuzz(num)
-    puts num
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 3).zero?
+      'fizz'
+    elsif (num % 5).zero?
+      'buzz'
+    else
+      num.to_s
+    end
   end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -4,25 +4,30 @@ require_relative '../solver'
 
 describe Solver do
   it 'should return the factorial of N' do
-    expect factorial(3).to eq(6)
+    factorial = Solver.new
+    expect(factorial.factorial(3)).to eq(6)
+    expect(factorial.factorial(0)).to eq(1)
+    expect { factorial.factorial(-1) }.to raise_error(StandardError)
   end
 
   it 'should return the reversed string' do
-    expect reverse('hello').to eq('olleh')
+    str_reverse = Solver.new.reverse('hello')
+    expect(str_reverse).to eql('olleh')
   end
 
   context 'Fizzbuzz test' do
+    fizzbuzz = Solver.new
     it 'should return fizz if N is divisible by 3' do
-      expect fizzbuzz(6).to be('fizz')
+      expect(fizzbuzz.fizzbuzz(6)).to be('fizz')
     end
     it 'should return buzz if N is divisible by 5' do
-      expect fizzbuzz(10).to be('buzz')
+      expect(fizzbuzz.fizzbuzz(10)).to be('buzz')
     end
     it 'should return fizzbuzz if N is divisible by 3 and 5' do
-      expect fizzbuzz(15).to be('fizzbuzz')
+      expect(fizzbuzz.fizzbuzz(15)).to be('fizzbuzz')
     end
     it "should return 'N' if N is not divisible by 3 or 5" do
-      expect fizzbuzz(8).to be('8')
+      expect(fizzbuzz.fizzbuzz(8)).to eq('8')
     end
   end
 end
