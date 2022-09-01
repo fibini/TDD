c1a623a00fa31ca6b8d1759bd865f8ef0c4df69e
Daf <dafne.azzolina@gmail.com>
2022-09-01 18:48:36 -0300

    Add Solver class

---
+++ solver.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# comment
+class Solver
+  def factorial(num)
+    puts num
+  end
+
+  def reverse(string)
+    puts string
+  end
+
+  def fizzbuzz(num)
+    puts num
+  end
+end
