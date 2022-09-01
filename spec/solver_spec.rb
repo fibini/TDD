d6ee777444fe53f4c6ba83fdce38695c2b37d1ca
Fabien <fabienswift@gmail.com>
2022-09-01 16:44:53 -0400

    Add second test

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -4,4 +4,8 @@ describe Solver do
     it 'should return the factorial of N' do
         expect (factorial(3)).to eq(6)
     end
+
+    it 'should return the reversed string' do
+        expect (reverse('hello')).to eq('olleh')
+    end
 end
